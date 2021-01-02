8d8dd9075dbcb26ec219bf0b8a7b2bfbcb408d7e
06kaushik <himanshu06kaushik@gmail.com>
2021-01-02 13:40:47 +0530

    init

--- function.js
+++ function.js
@@ -1,21 +1,58 @@
 function enableEditMode() {
-    richTextField.document.designMode = "on";
-  }
-  function Edit(command) {
-    richTextField.document.execCommand(command, false, null);
-  }
-  function execVal(command, value) {
-    richTextField.document.execCommand(command, false, value);
-  }
-  window.onscroll = function() {myFunction()};
+  richTextField.document.designMode = "on";
+}
+function Edit(command) {
+  richTextField.document.execCommand(command, false, null);
+}
+function execVal(command, value) {
+  richTextField.document.execCommand(command, false, value);
+}
+window.onscroll = function() {myFunction()};
 
-var toolbar = document.getElementById("toolbar");
-var sticky = toolbar.offsetTop;
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    toolbar.classList.add("sticky")
+    navbar.classList.add("sticky")
   } else {
-    toolbar.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
+
+(() =>{
+
+  if ('indexDb' in window){
+    console.log("browser is supporting indexDb");
+    return;
+  }
+
+  let button = document.querySelector(".btn");
+  button.addEventListener("click", AddData);
+
+})();
+
+function AddData(){
+
+  const dbname = "MyDB";
+ const requestDB= window.indexedDB.open(dbname);
+
+ requestDB.onupgradeneeded=()=>{
+   
+  let db=requestDB.result;
+   let store = db.createObjectStore("iframe",{autoIncreament:true});
+ 
+store.put({iframe:"Welcome to text editor"});
+
+
+  }
+
+   requestDB.onsuccess =()=>{
+     if(requestDB.readyState=="done"){
+       console.log("Data is insterted");
+     
+     }
+   }
+
+
+ }
